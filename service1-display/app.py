050df49aee806cea56dac7d340682ce59265d48a
Ordecaos <Jackwatkinson1996@hotmail.co.uk>
2021-12-02 12:23:38 +0000

    Secret key attempt

--- service1-display/app.py
+++ service1-display/app.py
@@ -8,7 +8,7 @@ import requests
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///char.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = ""
+#app.config['SECRET_KEY'] = ""
 
 db = SQLAlchemy(app)
 
